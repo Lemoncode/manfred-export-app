1aff94d00680ac07eeba510e136b1f38032dc647
Sergio Gordillo <sergiogordillodev@gmail.com>
2023-02-25 21:01:41 +0100

    trying to resolve alias issue but it doesnt work yet

--- apps/react-app/src/app.tsx
+++ apps/react-app/src/app.tsx
@@ -1,6 +1,7 @@
+import { RouterComponent } from 'core/routes/router.component';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { RouterComponent } from './core';
+
 
 export const App = () => {
   return <RouterComponent />;
--- apps/react-app/vite.config.ts
+++ apps/react-app/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: {
     modules: {
       localsConvention: 'camelCase',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@/*': path.resolve(__dirname, 'src/*'),
     },
   },
 });
