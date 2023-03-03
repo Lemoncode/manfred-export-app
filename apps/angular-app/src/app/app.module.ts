83b4179e6f2bb32bcae2f6d5db398300996fdb08
Braulio Diez Botella <braulio.diez@gmail.com>
2023-03-03 14:09:38 +0100

    material removed

--- apps/angular-app/src/app/app.module.ts
+++ apps/angular-app/src/app/app.module.ts
@@ -7,11 +7,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { TemplateSelectionComponent } from './pages/template-selection/template-selection.component';
 import { MenuComponent } from './layout/menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, TemplateSelectionComponent, MenuComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
