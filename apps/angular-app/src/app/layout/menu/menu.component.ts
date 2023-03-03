4cbf5fb5ce61ae123438cb9607523086ad3a1ff8
Braulio Diez Botella <braulio.diez@gmail.com>
2023-03-03 14:14:31 +0100

    fix menu component

--- apps/angular-app/src/app/layout/menu/menu.component.ts
+++ apps/angular-app/src/app/layout/menu/menu.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
