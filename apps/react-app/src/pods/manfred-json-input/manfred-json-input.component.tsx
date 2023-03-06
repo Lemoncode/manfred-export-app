282d07ffd3a972aea71576f77d20c4fd2e55d579
Abhinit Rai <abhinitrai@Abhinits-MacBook-Pro.local>
2023-03-06 19:23:58 +0530

    Issue #25 fixed

--- apps/react-app/src/pods/manfred-json-input/manfred-json-input.component.tsx
+++ apps/react-app/src/pods/manfred-json-input/manfred-json-input.component.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 export const ManfredJsonInput: React.FC = () => {
-  return <h1>Hello from ManfredJsonInputComponent</h1>;
+  const [textval, setTextVal] = useState("");
+  
+  function handleClick(){
+    if(textval.length>0){
+      alert("here will go the export to the word");
+    } else{
+      alert("No content");
+    }
+  }
+  return (
+    <>
+  <h1>Hello from ManfredJsonInputComponent</h1>
+  <textarea value={textval} onChange={(e) => setTextVal(e.target.value)}></textarea>
+  <br /><br />
+  <button onClick={handleClick}>Click me</button>
+  </>
+  )
 };
