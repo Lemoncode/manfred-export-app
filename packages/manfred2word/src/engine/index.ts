f76f21acd66a7a19ecf6a51950a2218b34e41890
Braulio Diez Botella <braulio.diez@gmail.com>
2023-03-13 08:39:02 +0100

    no funciona

--- packages/manfred2word/src/engine/index.ts
+++ packages/manfred2word/src/engine/index.ts
@@ -2,17 +2,14 @@ import { Document, Packer } from 'docx';
 import { download } from './engine.helpers';
 import { doc } from './mock-document';
 
-const createDocument = (): Promise<Document> => {
+const createMetaDocument = (): Promise<Document> => {
   return new Promise(resolve => {
     resolve(doc);
   });
 };
 
-export const exportManfredJSonToWord = () => {
-  createDocument().then(doc => {
-    Packer.toBlob(doc).then(blob => {
-      const filename = 'exportManfredJSonToWord.docx';
-      download(blob, filename);
-    });
-  });
+export const exportManfredJSonToWordAndDownload = async (filename: string) => {
+  const doc = await createMetaDocument();
+  const blob = await Packer.toBlob(doc);
+  download(blob, filename);
 };
