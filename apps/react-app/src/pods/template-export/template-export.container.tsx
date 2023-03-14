377e92f1672c24ebb7096262f3d9254a3db1346f
Braulio Diez Botella <braulio.diez@gmail.com>
2023-03-14 14:29:53 +0100

    fullpath client, pending lib

--- apps/react-app/src/pods/template-export/template-export.container.tsx
+++ apps/react-app/src/pods/template-export/template-export.container.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import { UserChoiceContext } from '@/core/user-choice/user-choice.context';
 import { TemplateExport } from './template-export.component';
-import { mockedManfredJsonProfile as mockedManfredJsonProfileStringFormat } from './mock';
 import { exportManfredJSonToWordAndDownload, parseStringToManfredJSon } from '@lemoncode/manfred2word';
 import { DEFAULT_EXPORT_FILENAME } from '@/core';
 import { useUserChoiceContext } from '@/core/user-choice';
 
 export const TemplateExportContainer: React.FC = () => {
   const { userChoice } = useUserChoiceContext();
-  console.log('datos del contexto', userChoice);
-
-  const [text, setText] = React.useState('');
-
-  React.useEffect(() => {
-    setText(mockedManfredJsonProfileStringFormat);
-  }, []);
 
   const handleExport = async () => {
-    const manfredJsonContent = parseStringToManfredJSon(text);
+    const manfredJsonContent = parseStringToManfredJSon(userChoice.manfredJsonContent);
     exportManfredJSonToWordAndDownload(DEFAULT_EXPORT_FILENAME, manfredJsonContent);
   };
 
