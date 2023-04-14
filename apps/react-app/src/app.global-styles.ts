303b8ae98119d6506a19ed5b5942fa229fed23e5
salino3 <flaviobcn012@gmail.com>
2023-04-14 12:31:42 +0200

    added more fonts styles

--- apps/react-app/src/app.global-styles.ts
+++ apps/react-app/src/app.global-styles.ts
@@ -1,19 +1,43 @@
 import { injectGlobal } from '@emotion/css';
 
 injectGlobal`
+* {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
 body {
-    margin: 0;
-    padding: 0;
     height: 100vh;
     display: flex;
     flex-direction: column;
     overflow: hidden;
     font-family: 'Inter', Roboto, 'Arial', 'Helvetica', sans-serif;
+    line-height: 1;
   }
 
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
+
 `;
