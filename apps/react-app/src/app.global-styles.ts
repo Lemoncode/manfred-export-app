a51f6762e0035e0594bf4794cbb2c347297b4bd4
salino3 <flaviobcn012@gmail.com>
2023-04-14 15:15:12 +0200

    add styles in app-global-styles.ts

--- apps/react-app/src/app.global-styles.ts
+++ apps/react-app/src/app.global-styles.ts
@@ -1,6 +1,35 @@
 import { injectGlobal } from '@emotion/css';
 
 injectGlobal`
+* {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
 body {
     margin: 0;
     padding: 0;
@@ -9,7 +38,7 @@ body {
     flex-direction: column;
     overflow: hidden;
     font-family: 'Inter', Roboto, 'Arial', 'Helvetica', sans-serif;
-    background-image: url("../public/assets/background_1.png");
+        background-image: url("../public/assets/background_1.png");
 
   }
 
@@ -18,4 +47,5 @@ body {
     margin: 0;
     padding: 0;
   }
+
 `;
