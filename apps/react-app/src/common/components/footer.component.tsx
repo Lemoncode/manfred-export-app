c2bed65e680a432e1ad8d27fcc81c7f462663f47
salino3 <flaviobcn012@gmail.com>
2023-04-18 09:46:25 +0200

    add div footer

--- apps/react-app/src/common/components/footer.component.tsx
+++ apps/react-app/src/common/components/footer.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { cx } from '@emotion/css';
 import * as classes from './footer.styles';
+import { Link } from 'react-router-dom';
 
 interface Props {
   className?: string;
@@ -11,7 +12,12 @@ export const Footer: React.FC<Props> = props => {
 
   return (
     <footer className={cx(classes.footerContainer, className)}>
-      <img src="./assets/Footer_text.png" alt="footer" />
+      {/* <img src="./assets/Footer_text.png" alt="footer" /> */}
+      <div className={classes.divText}>
+        Copyright © 2023
+        <br />
+        Creado por los alumnos del <span>Máster FrontEnd Lemoncode</span>
+      </div>
     </footer>
   );
 };
--- apps/react-app/src/common/components/footer.styles.ts
+++ apps/react-app/src/common/components/footer.styles.ts
@@ -2,9 +2,24 @@ import { css } from '@emotion/css';
 import { theme } from '@/core/theme';
 
 export const footerContainer = css`
-  img {
+  /* img {
     float: right;
     width: 120%;
     margin-bottom: 2em;
+  } */
+
+  h2 {
+    float: right;
+    font-size: larger;
+    color: ${theme.palette.primary[500]};
+
+    span {
+      color: ${theme.palette.warning[500]};
+    }
   }
+`;
+
+export const divText = css`
+  float: right;
+  margin-right: 2em;
 `;
