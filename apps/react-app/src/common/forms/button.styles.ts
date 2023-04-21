00babb57dc1d6dfc4b395cce610b0677d8fe4e19
salino3 <flaviobcn012@gmail.com>
2023-04-21 16:49:10 +0200

    delete margin styles

--- apps/react-app/src/common/forms/button.styles.ts
+++ apps/react-app/src/common/forms/button.styles.ts
@@ -1,48 +1,3 @@
-// import { css } from '@emotion/css';
-// import { theme } from '@/core/theme';
-// import { ColorPrincipal, ColorSecondary } from './mf-color.constants';
-
-// export const buttonStyle = css`
-//   background-color: ${theme.palette.info[500]};
-//   color: ${theme.palette.primary[700]};
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   align-items: center;
-//   padding: 8px 16px;
-//   box-sizing: border-box;
-
-//   /* gap: 10px; */
-//   /* max-width: 278px; */
-
-//   height: 32px;
-
-//   border: none;
-//   border-radius: 8px;
-//   font: ${theme.typography.button};
-
-//   &:hover {
-//     transition: background 0.25s ease-in-out;
-//     background-color: ${ColorSecondary};
-//   }
-
-//   @media (min-width: 1025px) {
-
-//     grid-area: button;
-//     margin: auto;
-//     display: grid;
-//     grid-template-columns: 1fr 1fr;
-//   }
-// `;
-
-// //!
-// export const icon = css`
-//   width: 20px;
-//   height: 14px;
-//   /* margin: 0px 8px; */
-//   margin: auto;
-// `;
-
 import { css } from '@emotion/css';
 import { theme } from '@/core/theme';
 import { ColorPrincipal, ColorSecondary } from './mf-color.constants';
@@ -74,7 +29,6 @@ export const buttonStyle = css`
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    margin: auto;
   }
 `;
 
