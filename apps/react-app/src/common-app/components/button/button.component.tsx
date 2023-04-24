2b4cbd069a805363d1654d0aa253f1fb00e915a9
salino3 <flaviobcn012@gmail.com>
2023-04-24 13:11:51 +0200

    delete HTMLButtonElement

--- apps/react-app/src/common-app/components/button/button.component.tsx
+++ apps/react-app/src/common-app/components/button/button.component.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { cx } from '@emotion/css';
 import * as classes from './button.styles';
 
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface Props {
   children?: React.ReactNode;
   showIcon?: boolean;
   className?: string;
+  onClick?: () => void;
 }
 
 export const Button: React.FC<Props> = props => {
-  const { showIcon = true, className, children } = props;
+  const { showIcon = true, className, children, onClick } = props;
   return (
-    <button className={cx(classes.buttonStyle, className)}>
+    <button onClick={onClick} className={cx(classes.buttonStyle, className)}>
       {children}
-      {showIcon && <img src="./assets/arrow_button_1.svg" alt="" />}
+      {showIcon && <img src="./assets/arrow_button_1.svg" alt="arrow" />}
     </button>
   );
 };
