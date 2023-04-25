6e4df5004f65e1649c83544517a5f9a104f531f1
salino3 <flaviobcn012@gmail.com>
2023-04-25 18:06:07 +0200

    add media queries in textarea component

--- apps/react-app/src/common-app/components/textarea/textarea.styles.ts
+++ apps/react-app/src/common-app/components/textarea/textarea.styles.ts
@@ -2,11 +2,21 @@ import { css } from '@emotion/css';
 import { theme } from '@/core/theme';
 
 export const textarea = css`
-  width: 1004px;
-  height: 442px;
+  width: 358px;
+  height: 538px;
   padding: 16px;
   box-sizing: border-box;
   border-radius: 8px;
   background-color: ${theme.palette.dark[700]};
   color: ${theme.palette.light[700]};
+
+  @media (min-width: 725px) {
+    width: 706px;
+    height: 661px;
+  }
+
+  @media (min-width: 1024px) {
+    width: 1004px;
+    height: 442px;
+  }
 `;
