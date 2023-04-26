a78264a284a44e1868c36f205fc61cfeae74d15d
Juan Berrocal <juan.brudner@google.com>
2023-04-26 06:19:40 +0200

    Add test case for fields with values

--- packages/manfred2word/src/engine/doc-parts/profile-section/profile-section.mapper.spec.ts
+++ packages/manfred2word/src/engine/doc-parts/profile-section/profile-section.mapper.spec.ts
@@ -1,6 +1,7 @@
 import { ManfredAwesomicCV, Person } from '@/model';
 import { mapFromMacCvToProfileSectionVm } from './profile-section.mapper';
 import { ProfileSectionVm, RelevantLink } from './profile-section.vm';
+import { ContactMean } from '../../../model/manfred-schema.model';
 
 describe('Testing profile-section.mapper.ts', () => {
   it('should pass spec', () => {
@@ -154,6 +155,7 @@ describe('Testing profile-section.mapper.ts', () => {
 
   it('It should returns empty cv when any field from aboutMe field is undefined', () => {
     // Arrange
+
     const person: any = {
       name: undefined,
       surnames: undefined,
@@ -184,6 +186,57 @@ describe('Testing profile-section.mapper.ts', () => {
       fullname: ' ',
       emails: [] as string[],
       revelantLinks: [] as RelevantLink[],
+    };
+
+    // Act
+    const result = mapFromMacCvToProfileSectionVm(cv);
+    // Assert
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('It should returns cv with proper data when fields from aboutMe field have valid values', () => {
+    // Arrange
+    const theContact: any = {
+      contactMails: ['john.doe@mydomain.com', 'john.doe@anydomain.com'],
+    };
+
+    const theLink: any = {
+      type: 'github',
+      URL: 'john.doe@github.com',
+      description: 'This is my link.',
+    };
+
+    const person: any = {
+      name: 'John',
+      surnames: 'Doe',
+      title: 'Computer Science Bachelor',
+      description: 'Frontend developer',
+      birthday: '30/03/1990',
+      avatar: undefined,
+      contact: theContact,
+      location: undefined,
+    };
+
+    const cv: ManfredAwesomicCV = {
+      settings: {
+        language: 'English',
+        lastUpdate: '25/04/2023',
+        MACVersion: '1.0',
+      },
+      aboutMe: {
+        profile: person,
+        relevantLinks: [theLink],
+      },
+    };
+
+    const expectedResult: ProfileSectionVm = {
+      name: 'John',
+      surnames: 'Doe',
+      title: 'Computer Science Bachelor',
+      description: 'Frontend developer',
+      fullname: 'John Doe',
+      emails: ['john.doe@mydomain.com', 'john.doe@anydomain.com'] as string[],
+      revelantLinks: [theLink] as RelevantLink[],
     };
 
     // Act
