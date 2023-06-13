dbd451f1762fa71b90851f88947d53b5ad9211a8
salino3 <flaviobcn012@gmail.com>
2023-06-13 10:29:31 +0200

    add generateSoftSkillSection to generateSections

--- packages/manfred2word/src/engine/doc-parts/index.ts
+++ packages/manfred2word/src/engine/doc-parts/index.ts
@@ -1,3 +1,4 @@
 export * from './profile-section/profile-section.part';
 export * from './experience-section/experience-section.part';
 export * from './language-section/language-section.part';
+export * from './soft-skill-section/soft-skill-section.part';
