a125e402525ae3d66f19a243644a97bf9e15c7c8
MTeresaMB <milanesteresa@gmail.com>
2023-06-15 13:31:54 +0200

    fix index.ts

--- packages/manfred2word/src/engine/doc-parts/index.ts
+++ packages/manfred2word/src/engine/doc-parts/index.ts
@@ -1,3 +1,4 @@
 export * from './profile-section/profile-section.part';
 export * from './experience-section/experience-section.part';
 export * from './language-section/language-section.part';
+export * from './studies-section/studies-section.part';
