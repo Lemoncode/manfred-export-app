2ecf0f6a85434c7054f144bd7b62860d78139916
salino3 <flaviobcn012@gmail.com>
2023-06-15 14:40:15 +0200

    changing texts

--- packages/manfred2word/src/engine/doc-parts/soft-skill-section/sections-soft-skill-section.parts.ts
+++ packages/manfred2word/src/engine/doc-parts/soft-skill-section/sections-soft-skill-section.parts.ts
@@ -6,9 +6,9 @@ export const titleSoftSkillSection = () => {
     spacing: { line: 400 },
     children: [
       new TextRun({ size: '14pt', font: 'Arial', break: 1 }),
-      new TextRun({ text: 'Habilidades/', size: '14pt', font: 'Arial' }),
+      new TextRun({ text: 'Soft Skills/', size: '14pt', font: 'Arial' }),
       new TextRun({
-        text: 'NIVEL',
+        text: 'Nivel',
         size: '14pt',
         bold: true,
         font: 'Arial',
@@ -21,9 +21,15 @@ export const sectionSoftSkillSection = (skillVM: SoftSkillVM) =>
   new Paragraph({
     spacing: { line: 400 },
     children: [
-      new TextRun({ text: skillVM?.skill?.name, size: '12pt', font: 'Arial', bold: true, break: 1 }),
       new TextRun({
-        text: skillVM?.skill?.description ? ': ' + skillVM?.skill?.description : '',
+        text: skillVM?.skill?.name + (skillVM?.skill?.description ? ': ' : ''),
+        size: '12pt',
+        font: 'Arial',
+        bold: true,
+        break: 1,
+      }),
+      new TextRun({
+        text: skillVM?.skill?.description ? skillVM?.skill?.description : '',
         size: '10pt',
         font: 'Arial',
       }),
