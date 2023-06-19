4ce7702729f9cee27f1e9fa2ba3f9f2561f94bd4
salino3 <flaviobcn012@gmail.com>
2023-06-19 10:36:15 +0200

    fix issues

--- apps/react-app/src/common-app/components/navbar/navbar.component.tsx
+++ apps/react-app/src/common-app/components/navbar/navbar.component.tsx
@@ -6,14 +6,16 @@ import * as classes from './navbar.styles';
 export const Navbar: React.FC = () => {
   const [hiddeMenu, setHiddeMenu] = React.useState(true);
 
-  const handleShowMenu = () => setHiddeMenu(current => !current);
+  const handleShowMenu = () => {
+    setHiddeMenu(current => !current);
+  };
 
   return (
     <nav className={classes.nav}>
       <div className={classes.logo}></div>
       <img className={classes.labelIcon} src="/assets/beta-icon.svg" alt="icono" />
       <img onClick={handleShowMenu} className={classes.iconMenu} src="/assets/lines-vector.svg" alt="menú" />
-      <Menu className={cx({ [classes.hidde]: hiddeMenu })} />
+      <Menu handleCloseMenu={handleShowMenu} className={cx({ [classes.hidde]: hiddeMenu })} />
     </nav>
   );
 };
