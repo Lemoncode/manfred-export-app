5d5a95c1500cc2a4a1eeebab35aa0be7f537927d
manudous <manudous@gmail.com>
2023-06-20 21:32:02 +0200

    wip

--- apps/react-app/src/common-app/components/navbar/navbar.styles.ts
+++ apps/react-app/src/common-app/components/navbar/navbar.styles.ts
@@ -3,23 +3,23 @@ import { theme } from '@/core/theme';
 
 export const nav = css`
   position: fixed;
-  z-index: 999;
+  display: flex;
+  height: 50px;
   top: 0;
   width: 100%;
-  display: flex;
   justify-content: space-between;
   padding: 0px ${theme.spacing(4)};
   align-items: center;
   background: ${theme.palette.dark[900]};
-  height: 50px;
   box-sizing: border-box;
+  z-index: 999;
 `;
 
 export const logo = css`
-  height: 32px;
   background-image: url('/assets/logo.svg');
   background-repeat: no-repeat;
   background-size: cover;
+  height: 32px;
   width: 32px;
 
   @media (min-width: 725px) {
