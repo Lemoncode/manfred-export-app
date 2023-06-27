d2ff190c4b8f3d6497ff5580b90251c609f7a973
manudous <manudous@gmail.com>
2023-06-27 14:33:42 +0200

    fix languages section

--- packages/manfred2word/src/engine/doc-parts/language-section/sections-languages-section-parts.ts
+++ packages/manfred2word/src/engine/doc-parts/language-section/sections-languages-section-parts.ts
@@ -1,27 +1,43 @@
 import { Paragraph, TextRun } from 'docx';
 import { LanguageVm } from './language-section.vm';
+import { generateLineSpacer } from '@/common-app';
 
-export const titleLanguageSection = () => {
-  return new Paragraph({
-    spacing: { line: 400 },
+export const titleLanguageSection = () =>
+  new Paragraph({
+    spacing: { before: 200 },
     children: [
-      new TextRun({ size: '14pt', font: 'Arial', break: 1 }),
-      new TextRun({ text: 'LENGUAJE/', size: '14pt', font: 'Arial' }),
+      new TextRun({ text: 'LENGUAJE/', size: '18pt' }),
       new TextRun({
         text: 'NIVEL',
-        size: '14pt',
+        size: '18pt',
         bold: true,
-        font: 'Arial',
       }),
     ],
   });
-};
 
-export const sectionLanguageSection = (languageVm: LanguageVm) =>
+const nameLanguageSection = (languageVm: LanguageVm): Paragraph =>
   new Paragraph({
-    spacing: { line: 400 },
+    spacing: { before: 200 },
+    children: [new TextRun({ text: languageVm.name, size: '12pt', bold: true })],
+  });
+
+const levelLanguageSection = (languageVm: LanguageVm): Paragraph =>
+  new Paragraph({
+    spacing: { before: 200 },
     children: [
-      new TextRun({ text: languageVm?.name, size: '12pt', font: 'Arial', bold: true, break: 1 }),
-      new TextRun({ text: languageVm?.level, size: '10pt', font: 'Arial', break: 1 }),
+      new TextRun({ text: 'Nivel: ', size: '12pt', bold: true }),
+      new TextRun({ text: languageVm?.level, size: '12pt' }),
     ],
   });
+
+export const sectionLanguageSection = (languageVm: LanguageVm): Paragraph[] => {
+  let languageSection: Paragraph[] = [];
+
+  languageSection = [...languageSection, nameLanguageSection(languageVm)];
+
+  if (languageVm?.level) {
+    languageSection = [...languageSection, levelLanguageSection(languageVm)];
+  }
+
+  return [...languageSection, generateLineSpacer()];
+};
