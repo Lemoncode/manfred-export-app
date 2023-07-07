1652b3fba7922768cc34f8f31b9ba58763689428
juanpms2 <juanpablo@lemoncode.net>
2023-07-07 09:17:02 +0200

    refactor

--- packages/manfred2word/src/engine/doc-parts/hard-skill-section/hard-skill-section.helpers.ts
+++ packages/manfred2word/src/engine/doc-parts/hard-skill-section/hard-skill-section.helpers.ts
@@ -1,14 +1,5 @@
 import { IParagraphOptions, IRunOptions, Paragraph, Table, TableCell, TableRow, TextRun, XmlComponent } from 'docx';
-import { HardSkillVM } from './hard-skill-section.vm';
-import { generateLineSpacer } from '@/common-app';
 import { TableStyles } from '../doc-parts.vm';
-
-export const renderTitleHardSkillSection = () => {
-  return new Paragraph({
-    spacing: { before: 200 },
-    children: [new TextRun({ text: 'Hard Skills', size: '18pt', bold: true })],
-  });
-};
 
 export const renderTable = (rows: TableRow[], styles?: TableStyles): XmlComponent =>
   new Table({
@@ -16,13 +7,13 @@ export const renderTable = (rows: TableRow[], styles?: TableStyles): XmlComponen
     rows: [...rows],
   });
 
-export const renderTableRow = (cells: TableCell[], styles?: TableStyles): XmlComponent =>
+export const renderTableRow = (cells: TableCell[], styles?: TableStyles): TableRow =>
   new TableRow({
     ...styles,
     children: [...cells],
   });
 
-export const renderTableCell = (children: XmlComponent[], styles?: TableStyles): XmlComponent =>
+export const renderTableCell = (children: XmlComponent[], styles?: TableStyles): TableCell =>
   new TableCell({
     ...styles,
     children: [...children],
--- packages/manfred2word/src/engine/doc-parts/hard-skill-section/hard-skill-section.part.ts
+++ packages/manfred2word/src/engine/doc-parts/hard-skill-section/hard-skill-section.part.ts
@@ -1,69 +1,47 @@
-import {
-  TableCell,
-  TableRow,
-  Table,
-  ITableCellOptions,
-  XmlComponent,
-  ITableRowOptions,
-  TextRun,
-  Paragraph,
-} from 'docx';
+import { TableRow, Table, XmlComponent, HeadingLevel } from 'docx';
 import { ManfredAwesomicCV } from '@/model';
 import { HardSkillVM } from './hard-skill-section.vm';
 import { mapFromCvToHardSkillVm } from './hard-skill-section.mapper';
+import {
+  renderTable,
+  renderTableRow,
+  renderTableCell,
+  renderParagraph,
+  renderTextRun,
+} from './hard-skill-section.helpers';
+import { sectionTitle } from './hard-skill-constants';
 import { styles } from './hard-skill-section.styles';
-import { renderTitleHardSkillSection } from './sections-hard-skill-section.parts';
-
-const createTable = (hardSkillList: HardSkillVM[]): Table =>
-  new Table({
-    ...styles.table,
-    rows: [createTableTitle(), createHardSkillListRow(hardSkillList)],
-  });
-
-const createTableTitle = (): TableRow =>
-  new TableRow({
-    children: [
-      new TableCell({
-        children: [renderTitleHardSkillSection()],
-      }),
-    ],
-  });
-
-const createHardSkillListRow = (sectionSoftSkillVm: HardSkillVM[]): TableRow =>
-  new TableRow({
-    children: [
-      new TableCell({
-        ...styles.table,
-        children: [createParagraph(sectionSoftSkillVm)],
-      }),
-    ],
-  });
 
 const getSkillNameList = (skillList: HardSkillVM[]): string[] =>
   skillList.map(item => (item.skill?.name ? item.skill?.name : ''));
 
 const createSkillListWithSeparator = (skillList: string[]): XmlComponent[] =>
-  skillList.map((item, index) => {
-    if (index === skillList.length - 1) {
-      return createSoftSkillItem(item);
-    }
-    return createSoftSkillItem(`${item} | `);
-  });
+  skillList.map((item, index) => (index === skillList.length - 1 ? renderTextRun(item) : renderTextRun(`${item} | `)));
 
-const createParagraph = (skillList: HardSkillVM[]): XmlComponent => {
+const createSkillsTableRow = (skillList: HardSkillVM[]): TableRow => {
   const list = getSkillNameList(skillList);
   const skillListWithSeparator = createSkillListWithSeparator(list);
+  const paragraph = renderParagraph(skillListWithSeparator, { spacing: { before: 200 } });
+  const cell = renderTableCell([paragraph]);
+  const row = renderTableRow([cell]);
 
-  return new Paragraph({
-    spacing: { before: 200 },
-    children: [...skillListWithSeparator],
-  });
+  return row;
 };
-const createSoftSkillItem = (skill: string): XmlComponent =>
-  skill ? new TextRun({ text: skill, size: '12pt' }) : new TextRun({ text: '', size: '12pt' });
+
+const createTitleTableRow = (text: string): TableRow => {
+  const title = renderTextRun(text, { size: '18pt', bold: true });
+  const paragraph = renderParagraph([title], { spacing: { before: 200 } });
+  const cell = renderTableCell([paragraph]);
+  const row = renderTableRow([cell]);
+
+  return row;
+};
 
 export const generateHardSkillSection = (cv: ManfredAwesomicCV): Table => {
   const hardSkillList = mapFromCvToHardSkillVm(cv);
+  const title = createTitleTableRow(sectionTitle);
+  const hardSkillItems = createSkillsTableRow(hardSkillList);
+  const table = renderTable([title, hardSkillItems], styles.table);
 
-  return createTable(hardSkillList);
+  return table;
 };
