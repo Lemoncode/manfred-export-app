bb70d263df71bf22f30b911e8680909a7c00ae75
salino3 <flaviobcn012@gmail.com>
2023-07-12 09:48:31 +0200

    create onExportJsonToMarkdown function

--- apps/react-app/src/pods/template-export/template-export.container.tsx
+++ apps/react-app/src/pods/template-export/template-export.container.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { exportManfredJSonToWordAndDownload, parseStringToManfredJSon } from '@lemoncode/manfred2word';
 import { DEFAULT_EXPORT_FILENAME } from '@/core';
 import { TemplateExport } from './template-export.component';
+import { onExportJsonToMarkdown } from '@/common-app/helpers';
 
 export const TemplateExportContainer: React.FC = () => {
   const onSetManfredJson = async (text: string) => {
     try {
       JSON.parse(text);
       const manfredJsonContent = parseStringToManfredJSon(text);
-      await exportManfredJSonToWordAndDownload(DEFAULT_EXPORT_FILENAME, manfredJsonContent);
+
+      const x: any = await exportManfredJSonToWordAndDownload(DEFAULT_EXPORT_FILENAME, manfredJsonContent);
+      const blob = new Blob([x], { type: 'text/markdown' });
+      onExportJsonToMarkdown(blob, 'archivo.md');
     } catch (error) {
       alert('Hay un error, no está utilizando el formato correcto');
     }
