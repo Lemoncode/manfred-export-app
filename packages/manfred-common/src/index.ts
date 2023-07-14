c7b59483c978971dcbb94535c3df3a26cd2c72cc
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-07-14 10:06:28 +0200

    enable sourcemap just in dev

--- packages/manfred-common/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const holaLib = () => console.log('hola lib common');
--- packages/manfred2word/vite.config.ts
+++ packages/manfred2word/vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { fileURLToPath } from 'node:url';
 
-export default defineConfig({
-  plugins: [dts()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'Manfred2Word',
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [dts()],
+    build: {
+      lib: {
+        entry: 'src/index.ts',
+        name: 'Manfred2Word',
+      },
+      sourcemap: mode === 'development',
     },
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-});
+  });
