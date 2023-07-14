4c1caec5f3e20d65f52c62fa09d4f8f0fc554716
juanpms2 <juanpablo@lemoncode.net>
2023-07-14 08:41:42 +0200

    renamed images

---
+++ packages/manfred2md/src/engine/constants.ts
@@ -0,0 +1,14 @@
+// TODO: update urls when they are in main branch
+
+export const profileSectionImg = {
+  github:
+    'https://raw.githubusercontent.com/Lemoncode/manfred-export-app/feature/%23128-add-profile-section-to-md/media/github.png',
+  twitter:
+    'https://raw.githubusercontent.com/Lemoncode/manfred-export-app/feature/%23128-add-profile-section-to-md/media/tweeter.png',
+  linkedin:
+    'https://raw.githubusercontent.com/Lemoncode/manfred-export-app/feature/%23128-add-profile-section-to-md/media/linkedin.png',
+  navigator:
+    'https://raw.githubusercontent.com/Lemoncode/manfred-export-app/feature/%23128-add-profile-section-to-md/media/navegador.png',
+  website:
+    'https://raw.githubusercontent.com/Lemoncode/manfred-export-app/feature/%23128-add-profile-section-to-md/media/sitio-web.png',
+};
