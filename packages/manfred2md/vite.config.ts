e10da02611f13cce082e7b45324385f0ca2082e8
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-07-14 09:34:57 +0200

    update turbo repo config and remove unneeded vite configs

--- packages/manfred2md/vite.config.ts
+++ packages/manfred2md/vite.config.ts
@@ -5,12 +5,9 @@ import { fileURLToPath } from 'node:url';
 export default defineConfig({
   plugins: [dts()],
   build: {
-    emptyOutDir: false,
     lib: {
       entry: 'src/index.ts',
       name: 'Manfred2Md',
-      fileName: format => `manfred2md.${format}.js`,
-      formats: ['es', 'cjs'],
     },
     sourcemap: true,
   },
--- packages/manfred2word/vite.config.ts
+++ packages/manfred2word/vite.config.ts
@@ -5,12 +5,9 @@ import { fileURLToPath } from 'node:url';
 export default defineConfig({
   plugins: [dts()],
   build: {
-    emptyOutDir: false,
     lib: {
       entry: 'src/index.ts',
       name: 'Manfred2Word',
-      fileName: format => `manfred2word.${format}.js`,
-      formats: ['es', 'cjs'],
     },
     sourcemap: true,
   },
