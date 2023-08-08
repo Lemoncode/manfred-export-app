48f7486d67f90304da2c5c07dbcefc08c0631933
JuanBerrocal <juan.brudner@gmail.com>
2023-08-09 00:03:05 +0200

    hard skills section OK

--- packages/manfred2md/src/engine/md-parts/index.ts
+++ packages/manfred2md/src/engine/md-parts/index.ts
@@ -2,3 +2,4 @@ export * from './experience-section';
 export * from './profile-section';
 export * from './studies-section';
 export * from './language-section';
+export * from './hard-skills-section';
