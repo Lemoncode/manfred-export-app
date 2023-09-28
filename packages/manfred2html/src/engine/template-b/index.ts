4ef5fd3ae3a241f6e4bf365d81051f7663335fef
migalko94 <miguelgalvezugr@gmail.com>
2023-09-28 11:55:49 +0200

    implement soft skills section to ejs

--- packages/manfred2html/src/engine/template-b/index.ts
+++ packages/manfred2html/src/engine/template-b/index.ts
@@ -47,6 +47,7 @@ export const exportManfredJSonToHTMLTemplateB = (
       ${relevantsLinksSection}
       ${languageSection}
       ${hardSkillsSection}
+      ${softSkillsSection}
       ${asideElementEnd}
       ${mainElementStart}
         
