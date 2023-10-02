62016b7e94e03efa5e19109802078f8b74013ba3
migalko94 <miguelgalvezugr@gmail.com>
2023-10-02 14:17:28 +0200

    adapt

---
+++ packages/manfred2html/src/engine/template-b/html-parts/aside-element-start/index.ts
@@ -0,0 +1,1 @@
+export * from './aside-element-start.part';
--- packages/manfred2html/src/engine/template-b/index.ts
+++ packages/manfred2html/src/engine/template-b/index.ts
@@ -1,16 +1,27 @@
 import { ManfredAwesomicCV } from '@/model';
 
+import {
+  generateHtmlDocumentStart,
+  generateHtmlDocumentEnd,
+  generateAsideElementStart,
+  generateAsideElementEnd,
+  generateMainElementStart,
+  generateMainElementEnd,
+} from './html-parts';
+
 export const exportManfredJSonToHTMLTemplateB = (manfredJsonContent: ManfredAwesomicCV, theme?: string): string => {
+  const htmlDocumentStart = generateHtmlDocumentStart();
+  const htmlDocumentEnd = generateHtmlDocumentEnd();
+  const asideElementStart = generateAsideElementStart();
+  const asideElementEnd = generateAsideElementEnd();
+  const mainElementStart = generateMainElementStart();
+  const mainElementEnd = generateMainElementEnd();
+
   return `
     ${htmlDocumentStart}
-      
       ${asideElementStart}
-      ${headerElementStart}
-       
-      ${headerElementEnd}
       ${asideElementEnd}
       ${mainElementStart}
-        
       ${mainElementEnd}
     ${htmlDocumentEnd}
   `;
