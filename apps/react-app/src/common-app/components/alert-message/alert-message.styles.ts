8d1e8461bfaaa23ab80d9108b34bad1ae338bb57
Abel <abeldetena@gmail.com>
2023-10-16 17:21:29 +0200

    Update alert-messafe.styles

--- apps/react-app/src/common-app/components/alert-message/alert-message.styles.ts
+++ apps/react-app/src/common-app/components/alert-message/alert-message.styles.ts
@@ -7,23 +7,25 @@ export const alertStyle = css`
   flex-direction: column;
 
   .modal-header {
-    background: linear-gradient(90deg, #414042 0%, #231f20 100%);
+    background: ${theme.palette.error[500]};
     height: 22px;
     border-radius: 10px 10px 0px 0px;
   }
 
   .modal-content {
-    background: linear-gradient(0deg, white 0%, #a7a9ac 100%);
-    border-radius: 0px 0px 15px 15px;
+    height: 173px;
+    background-color: ${theme.palette.info[50]};
+    border-radius: 0px 0px 10px 10px;
+    padding-top: ${theme.spacing(4)};
     display: flex;
-    padding-top: ${theme.spacing(3)};
-    padding-right: ${theme.spacing(4)};
-    padding-bottom: ${theme.spacing(2)};
+    gap: ${theme.spacing(2)};
   }
 
   img {
     width: 125px;
+    object-fit: cover;
   }
+
   .modal-main {
     display: flex;
     flex-direction: column;
@@ -31,79 +33,70 @@ export const alertStyle = css`
   }
 
   .modal-text {
+    display: flex;
     flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    display: inline-flex;
+    gap: ${theme.spacing(2)};
+    padding-right: ${theme.spacing(4)};
   }
 
   .modal-text--h1 {
-    ${theme.typography.mobile.h1};
-    color: #3f3f3f;
+    color: ${theme.palette.dark[900]};
     font-size: 32px;
-    font-family: Inter;
     font-weight: 500;
     word-wrap: break-word;
   }
 
-  .text-container h2 {
-    ${theme.typography.mobile.h5};
-    color: #7f7f7f;
-    font-size: 14px;
+  .modal-text--h2 {
+    color: ${theme.palette.info[900]};
     font-family: Inter;
+    font-size: 14px;
     font-weight: 400;
     word-wrap: break-word;
   }
 
   .modal-text--p {
-    ${theme.typography.mobile.h5};
-    color: black;
+    color: ${theme.palette.dark[900]};
     font-size: 8px;
-    font-family: Inter;
     font-weight: 400;
     word-wrap: break-word;
   }
 
   .modal-button {
     align-self: flex-end;
+    margin-right: ${theme.spacing(5)};
+    margin-bottom: ${theme.spacing(2)};
   }
 
   @media (min-width: 728px) {
     width: 548px;
-    height: 303px;
 
     .modal-header {
       height: 34px;
     }
 
+    .modal-content {
+      height: 270px;
+      padding-top: ${theme.spacing(6)};
+      gap: ${theme.spacing(4)};
+    }
+
     img {
       width: 195px;
     }
+
     .modal-text--h1 {
-      ${theme.typography.desktop.h1};
-      color: #3f3f3f;
       font-size: 40px;
-      font-family: Inter;
-      font-weight: 500;
-      word-wrap: break-word;
     }
-
-    .text-container h2 {
-      ${theme.typography.desktop.h2};
-      color: #7f7f7f;
+    .modal-text--h2 {
       font-size: 16px;
-      font-family: Inter;
-      font-weight: 400;
-      word-wrap: break-word;
+    }
+    .modal-text--p {
+      font-size: 12px;
     }
 
-    .modal-text--p {
-      ${theme.typography.desktop.h5};
-      color: black;
-      font-size: 12px;
-      font-family: Inter;
-      font-weight: 400;
-      word-wrap: break-word;
+    .modal-button {
+      margin-right: ${theme.spacing(8)};
+      margin-bottom: ${theme.spacing(5)};
     }
   }
 `;
