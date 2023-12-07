2cb3a327f6875a8140dd35a0996b27ed800232a1
MTeresaMB <milanesteresa@gmail.com>
2023-12-07 23:09:45 +0100

    cv-x-wing-squadron-create-header-section

--- packages/manfred2html/src/engine/cv-x-wing-squadron/index.ts
+++ packages/manfred2html/src/engine/cv-x-wing-squadron/index.ts
@@ -1,12 +1,24 @@
 import { ManfredAwesomicCV } from '@/model';
-import { generateHtmlDocumentStart, generateHtmlDocumentEnd } from './html-parts';
+import {
+  generateHtmlDocumentStart,
+  generateHtmlDocumentEnd,
+  generateHeaderElementEnd,
+  generateHeaderElementStart,
+  generateHeaderSection,
+} from './html-parts';
 
 export const exportManfredJsonToCVXWingHTML = (manfredJsonContent: ManfredAwesomicCV): string => {
   const htmlDocumentStart = generateHtmlDocumentStart();
   const htmlDocumentEnd = generateHtmlDocumentEnd();
+  const headerElementStart = generateHeaderElementStart();
+  const headerElementEnd = generateHeaderElementEnd();
+  const headerSection = generateHeaderSection(manfredJsonContent);
 
   return `
     ${htmlDocumentStart}
+      ${headerElementStart}
+      ${headerSection}
+      ${headerElementEnd}
     ${htmlDocumentEnd}
   `;
 };
