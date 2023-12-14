cec3bab1348304f28609d9ab055f32c20a30061d
MTeresaMB <milanesteresa@gmail.com>
2023-12-14 21:00:23 +0100

    add language labels

--- packages/manfred2html/src/engine/cv-x-wing-squadron/index.ts
+++ packages/manfred2html/src/engine/cv-x-wing-squadron/index.ts
@@ -1,4 +1,4 @@
-import { ManfredAwesomicCV } from '@/model';
+import { ManfredAwesomicCV, Settings } from '@/model';
 import {
   generateHtmlDocumentStart,
   generateHtmlDocumentEnd,
@@ -15,7 +15,7 @@ import {
   generateStudiesSection,
 } from './html-parts';
 
-export const exportManfredJsonToCVXWingHTML = (manfredJsonContent: ManfredAwesomicCV): string => {
+export const exportManfredJsonToCVXWingHTML = (manfredJsonContent: ManfredAwesomicCV, settings: Settings): string => {
   const htmlDocumentStart = generateHtmlDocumentStart();
   const htmlDocumentEnd = generateHtmlDocumentEnd();
   const headerElementStart = generateHeaderElementStart();
@@ -28,7 +28,7 @@ export const exportManfredJsonToCVXWingHTML = (manfredJsonContent: ManfredAwesom
   const hardSkillsSection = generateHardSkillsSection(manfredJsonContent);
   const softSkillsSection = generateSoftSkillsSection(manfredJsonContent);
   const experienceSection = generateExperienceSection(manfredJsonContent);
-  const studiesSection = generateStudiesSection(manfredJsonContent);
+  const studiesSection = generateStudiesSection(manfredJsonContent, settings);
 
   return `
     ${htmlDocumentStart}
