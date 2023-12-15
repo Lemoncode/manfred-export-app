4e5d002d0431c8e8f933e3c67a4533ef9807325d
MTeresaMB <milanesteresa@gmail.com>
2023-12-15 11:15:23 +0100

    add labels relevants links and studies section

---
+++ packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/relevants-links-section/relevants-links-section.part.ts
@@ -0,0 +1,15 @@
+import { ManfredAwesomicCV } from '@/model';
+import { ProfileSectionVm, mapFromMacCvToProfileSectionVm } from '@lemoncode/manfred-common/profile-section';
+import ejs from 'ejs';
+import relevantLinksTemplate from './relevant-links-section.ejs?raw';
+
+export const generateRelevantLinksSection = (cv: ManfredAwesomicCV): string => {
+  const profileSectionVm = mapFromMacCvToProfileSectionVm(cv);
+  return generateRelevantLinksSectionInner(profileSectionVm);
+};
+const generateRelevantLinksSectionInner = (profileSectionVm: ProfileSectionVm): string => {
+  const rootObject = {
+    profile: profileSectionVm,
+  };
+  return ejs.render(relevantLinksTemplate, rootObject);
+};
