c22f229c3a59d0779a8bb24ea1f86d1f651e27c2
MTeresaMB <milanesteresa@gmail.com>
2023-12-15 11:21:48 +0100

    fixed relevant links section ejs

--- packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/relevants-links-section/relevants-links-section.part.ts
+++ packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/relevants-links-section/relevants-links-section.part.ts
@@ -1,7 +1,7 @@
 import { ManfredAwesomicCV } from '@/model';
 import { ProfileSectionVm, mapFromMacCvToProfileSectionVm } from '@lemoncode/manfred-common/profile-section';
 import ejs from 'ejs';
-import relevantLinksTemplate from './relevant-links-section.ejs?raw';
+import relevantLinksTemplate from './relevants-links-section.ejs?raw';
 
 export const generateRelevantLinksSection = (cv: ManfredAwesomicCV): string => {
   const profileSectionVm = mapFromMacCvToProfileSectionVm(cv);
