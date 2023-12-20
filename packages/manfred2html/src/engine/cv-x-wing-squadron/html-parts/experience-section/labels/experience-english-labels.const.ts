d0e2bd7598dabfd3b93e36f5d763c754ad089a11
MTeresaMB <milanesteresa@gmail.com>
2023-12-20 20:15:24 +0100

    resolve experience links and add labels

--- packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/experience-section/labels/experience-english-labels.const.ts
+++ packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/experience-section/labels/experience-english-labels.const.ts
@@ -2,4 +2,6 @@ import { ExperienceLabels } from './experience-label.model';
 
 export const englishExperienceLabels: ExperienceLabels = {
   EXPERIENCE_CAREER_HEADING: 'Experience / Career',
+  TECH_TOOLS_HEADING: 'Technologies and Tools',
+  CURRENT_DATE: 'Now',
 };
--- packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/experience-section/labels/experience-spanish-labels.const.ts
+++ packages/manfred2html/src/engine/cv-x-wing-squadron/html-parts/experience-section/labels/experience-spanish-labels.const.ts
@@ -2,4 +2,6 @@ import { ExperienceLabels } from './experience-label.model';
 
 export const spanishExperienceLabels: ExperienceLabels = {
   EXPERIENCE_CAREER_HEADING: 'Experiencia / Carrera',
+  TECH_TOOLS_HEADING: 'Tecnologias y Herramientas',
+  CURRENT_DATE: 'Actualidad',
 };
