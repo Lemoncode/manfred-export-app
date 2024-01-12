4d0687e574d5941a06a28e778096611a03eb6036
Abel <abeldetena@gmail.com>
2024-01-12 13:41:13 +0100

    merge

--- packages/manfred2html/src/engine/index.ts
+++ packages/manfred2html/src/engine/index.ts
@@ -3,14 +3,12 @@ import { mapExportHTMLSettingsToSettings, mapExportHTMLJediSettingsToSettings } 
 import { exportManfredJSonToCVSithEleganceHTML } from './cv-sith-elegance';
 import { exportManfredJSonToCVGalacticCleanTechHTML } from './cv-galactic-cleantech';
 import { exportManfredJSonToCVJediMinimalismHTML } from './cv-jedi-minimalism';
-import { exportManfredJSonToCVJediMinimalismHTML } from './cv-jedi-minimalism';
 
 export const exportHTMLTemplate = (
   manfredJsonContent: ManfredAwesomicCV,
   exportHTMLSettings: ExportHTMLSettings
 ): string => {
   const settings = mapExportHTMLSettingsToSettings(exportHTMLSettings);
-  const Jedisettings = mapExportHTMLJediSettingsToSettings(exportHTMLSettings);
 
   switch (settings.template) {
     case 'Sith Elegance':
